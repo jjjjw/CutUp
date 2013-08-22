2efc70080191c1f13c25f11b9c7153776d7ecbba
JJ Weber <jjweber@gmail.com>
2013-08-21 23:25:12 -0700

    I have no idea what I'm doing commit

---
+++ CutUp.hs
@@ -0,0 +1,23 @@
+module CutUp() where
+import Data.Hashable
+import Data.List
+
+
+hashFlip :: String -> Bool
+hashFlip el = mod hashedEl 2 > 0
+    where hashedEl = hash el
+
+
+blockify :: [String] -> [String]
+blockify li = takeWhile hashFlip li
+
+
+consumeBlock :: [String] -> Maybe([String], [String])
+consumeBlock li = Just(block, remainder)
+    where block = blockify li
+          blockLen = length block
+          remainder = drop blockLen li
+
+
+makeBlocks :: [String] -> [[String]]
+makeBlocks li = unfoldr consumeBlock li

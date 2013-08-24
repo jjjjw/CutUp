0563189648a7eebf3ec1c720adbcf1ee5adcb228
JJ Weber <jjweber@gmail.com>
2013-08-24 15:39:20 -0700

    Nothing to see here

--- CutUp.hs
+++ CutUp.hs
@@ -8,36 +8,8 @@ import System.Random
 moduleRanGen = (mkStdGen 42)  -- Meaningful seed
 
 
-flips :: [Int]
-flips = randomRs (0, 9) moduleRanGen
-
-
-coinFlip :: StdGen -> (Bool, StdGen)
-coinFlip g el = mod (next g) 10 > 3
-
-
-takeWhileLucky :: ([String], StdGen) -> ([String], StdGen)
-takeWhileLucky ([], initG) = ([], initG)
-takeWhileLucky ((x:xs), initG)
-               | heads = (x, g) : takeWhileLucky (xs, g)  -- WTF?
-               | otherwise =  ([], initG)
-               where (heads, g) = coinFlip initG
-
-
-blockify :: ([String], StdGen) -> ([String], StdGen)
-blockify (l, g) = takeWhileLucky (l, g)
-
-
-consumeBlock :: ([String], StdGen) -> Maybe([String], ([String], StdGen))
-consumeBlock ([], initG) = Nothing
-consumeBlock (l, initG) = Just(block, (remainder, g))
-    where (block, g) = blockify (l, initG)
-          blockLen = length block
-          remainder = drop blockLen l
-
-
 makeBlocks :: [String] -> [[String]]
-makeBlocks l = unfoldr consumeBlock (l, moduleRanGen)
+makeBlocks l = unfoldr consumeBlock l
 
 
 -- http://www.haskell.org/haskellwiki/Random_shuffle: Section 3.1

b0a49a9cac55df7d858f6521bb71cd1c70567e7b
JJ Weber <jjweber@gmail.com>
2013-09-02 21:36:22 -0700

    Arg, lines are the better way to split

--- FoldIn.hs
+++ FoldIn.hs
@@ -1,25 +1,21 @@
 module FoldIn(foldIn) where
 import Data.List.Split
 
--- Create pseudo-lines from text, 15 characters per "line".
-lineize :: String -> [String]
-lineize = chunk 15
-
 
 fold :: String -> ([String], [String])
 fold s = bisected
   where
-    page = lineize s
+    page = lines s
     midpoint = div (length page) 2
     bisected = splitAt midpoint page
 
 
 foldIn :: String -> String -> String
-foldIn s1 s2 = concat spliced
+foldIn s1 s2 = unlines spliced
   where
     spliced = fst (fold s1) ++ snd (fold s2)
 
 
 main =
-  print (foldIn "The sky above the port was the color of television, tuned to a dead channel. It's not like I'm using,\" Case heard someone say, as he shouldered his way through the crowd around the door of the Chat. \"It's like my body's developed this massive drug deficiency.\"It was a Sprawl voice and a Sprawl joke. The Chatsubo was a bar for professional expatriates; you could drink there for a week and never hear two words in Japanese."
-    "Call me Ishmael. Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.")
+  print (foldIn "The sky above the port was the color of television, tuned to a dead channel.\n It's not like I'm using,\" Case heard someone say, as he shouldered his way through the crowd around the door of the Chat. \"It's like my body's developed this massive drug deficiency.\"It was a Sprawl voice and a Sprawl joke. The Chatsubo was a bar for professional expatriates; you could drink there for a week and never hear two words in Japanese."
+    "Call me Ishmael.\n Some years ago - never mind how long precisely - having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.")
